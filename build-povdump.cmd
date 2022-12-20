bcaafea2a328332923307c257c08821bca72cd16
slair <slair@users.noreply.github.com>
2022-12-20 17:49:31 +0400

    autocommit

--- build-povdump.cmd
+++ build-povdump.cmd
@@ -1,5 +1,6 @@
 @echo off
-
+:: -*- coding: cp866 -*-
+setlocal enabledelayedexpansion enableextensions
 :: sources folder
 set ORG=povray-3.8.0-beta.2
 
@@ -22,12 +23,11 @@ call apply-patch.cmd
 
 call make-patch.cmd
 
-del %MY%\windows\vs2015\bin64\pvengine64.exe > NUL 2>&1
-del %MY%\windows\vs2015\bin64\cmedit64.dll > NUL 2>&1
-
 pushd %MY%\windows\vs2015
 
 if not defined VSCMD_VER call vcvars64.bat
+del bin64\pvengine64.exe > NUL 2>&1
+del bin64\cmedit64.dll > NUL 2>&1
 MSBuild povray.sln -p:Configuration=Release -p:Platform="x64" -nologo
 
 del %POVDUMP_BIN_DIR%\povdump64.exe > NUL 2>&1
@@ -38,26 +38,61 @@ ln bin64/cmedit64.dll %POVDUMP_BIN_DIR%\cmedit64.dll > NUL 2>&1
 
 popd
 
+echo ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿
+call :povdump_test result !C:\slair\work.local\povdump\test.pov!
+echo ³ %result%
+echo ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ
+if %result%==failed goto :failed
+
+echo ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿
+call :povdump_test result !C:\slair\work.local\povray\my-blocks\main.pov!
+echo ³ %result%
+echo ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ
+if %result%==failed goto :failed
+
+:ok
+echo   _____________ ____________ _________ ___________ _________ ________________________ ___.____
+echo  /   _____/    ³   \_   ___ \\_   ___ \\_   _____//   _____//   _____/\_   _____/    ³   \    ³
+echo  \_____  \³    ³   /    \  \//    \  \/ ³    __)_ \_____  \ \_____  \  ³    __) ³    ³   /    ³
+echo  /        \    ³  /\     \___\     \____³        \/        \/        \ ³     \  ³    ³  /³    ³___
+echo /_______  /______/  \______  /\______  /_______  /_______  /_______  / \___  /  ³______/ ³_______ \
+echo         \/                 \/        \/        \/        \/        \/      \/                    \/
+echo.
+mpv.com %HOME%\share\sounds\jingl-victory.mp3 > NUL 2>&1
+goto :end
+:failed
+echo.
+echo ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿
+echo ³ BUILD FAILED ³
+echo ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ
+mpv.com %HOME%\share\sounds\trombone-sad.wav > NUL 2>&1
+goto :end
+:povdump_test  <resultVar> <pathVar>
+set POVDUMP_TEST_DIR=%~dp2
+set POVDUMP_TEST_FILE=%~nx2
+echo Testing '%POVDUMP_TEST_FILE%' in '%POVDUMP_TEST_DIR%'
+
+pushd %POVDUMP_TEST_DIR%
 del scene.dump > NUL 2>&1
 del povdump.log > NUL 2>&1
 
 if exist %POVDUMP_BIN_DIR%\povdump64.exe (
-povdump64.exe /EXIT +NRE +Itest.pov
-type scene.dump | hexdump.lua
-type povdump.log
+povdump64.exe /EXIT +NRE +I%POVDUMP_TEST_FILE%
+if exist scene.dump (
+head -c256 scene.dump | hexdump.lua
+::~ type povdump.log
+tail -n10 povdump.log
+set "%~1=ok"
+popd
+exit /b
 )
-
-if not exist scene.dump (
-echo.
-echo ===================
-echo ^| BUILD FAILED!!! ^|
-echo ===================
-mpv.com %HOME%\share\sounds\trombone-sad.wav > NUL 2>&1
-goto :end
+set "%~1=failed"
+popd
+exit /b
 )
-
-echo.
-echo BUILD SUCCESSFUL!!!
-mpv.com %HOME%\share\sounds\jingl-victory.mp3 > NUL 2>&1
+set "%~1=failed"
+popd
+exit /b
 
 :end
+echo.
