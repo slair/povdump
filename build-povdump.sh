8acedec8e0f6f4d38274e1f58e255dad855ae125
slair <slair@users.noreply.github.com>
2023-01-28 01:30:30 +0400

    autocommit

--- build-povdump.sh
+++ build-povdump.sh
@@ -11,6 +11,8 @@ POVVERSIONSYS=v3.8-beta
 #~ POVDUMP_BIN_DIR=POV-Ray/${POVVERSIONSYS}/bin
 POVDUMP_BIN_DIR=${HOME}/bin
 
+POVRAY_COMPILED_BY="enter_your_name <mail@host>"
+
 PATCHES_DIR=patches
 
 # check for sources
@@ -35,5 +37,50 @@ fi
 
 # let's build them
 echo "let's build them"
+
+set +u
+if [ -z "$ARCH" ]; then
+  case "$( uname -m )" in
+    i?86) ARCH=i486 ;;
+    arm*) ARCH=arm ;;
+       *) ARCH=$( uname -m ) ;;
+  esac
+fi
+set -u
+# povray prefers -O3 to build, so we do not use the -O2 flag
+if [ "$ARCH" = "i486" ]; then
+  SLKCFLAGS="-march=i486 -mtune=i686"
+  LIBDIRSUFFIX=""
+  BINNAME="povdump"
+elif [ "$ARCH" = "i686" ]; then
+  SLKCFLAGS="-march=i686 -mtune=i686"
+  LIBDIRSUFFIX=""
+  BINNAME="povdump"
+elif [ "$ARCH" = "x86_64" ]; then
+  SLKCFLAGS="-fPIC"
+  LIBDIRSUFFIX="64"
+  BINNAME="povdump64"
+else
+  SLKCFLAGS="-O2"
+  LIBDIRSUFFIX=""
+  BINNAME="povdump"
+fi
+
+echo "Configuring..."
+CXXFLAGS="${SLKCFLAGS}" \
+./configure \
+--prefix=/usr \
+--libdir=/usr/lib${LIBDIRSUFFIX} \
+--sysconfdir=/etc \
+--localstatedir=/var \
+--mandir=/usr/man \
+--build=$ARCH-slackware-linux \
+LIBS="-lboost_system -lboost_thread" \
+COMPILED_BY="${POVRAY_COMPILED_BY}"
+
+echo "Making..."
+make -s -j2
+cd unix
+mv povray ${BINNAME}
 
 echo "done"
